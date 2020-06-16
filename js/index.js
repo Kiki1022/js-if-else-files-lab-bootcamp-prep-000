d0d872ba7ddee66ab9a6eb9a9cb02bbd92935614
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-06-16 19:30:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,9 +4,3 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name === "Susan") {
-  cosole.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
